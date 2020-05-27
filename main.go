cd1468bc539c274ffb696428d08376743239634b
황병철 <bottleiron.h@gmail.com>
2020-05-27 17:40:20 +0900

    디렉토리 구조변경

--- main.go
+++ main.go
@@ -1,64 +1,61 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-var db = make(map[string]string)
+type User struct {
+	Admin_id string
+}
 
 func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
+
 	r := gin.Default()
-	// Ping test
+
+	r.LoadHTMLGlob("templates/**/*")
+	r.Static("assets/", "./assets")
 
 	r.GET("/", func(c *gin.Context) {
-		fmt.Println(http.StatusOK)
-		c.String(http.StatusOK, "main.go")
+
+		db, err := sql.Open("mysql", "nvp_planty:planty@tcp(1.255.153.228:3306)/nvp_svc")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer db.Close()
+
+		rows, err := db.Query("select admin_id from tbl_admin")
+		if err != nil {
+			fmt.Println("Error running query")
+			fmt.Println(err)
+			return
+		}
+		defer rows.Close()
+
+		var users []User
+		var userOnce User
+
+		for rows.Next() {
+			err := rows.Scan(&userOnce.Admin_id)
+			if err != nil {
+				fmt.Println(err)
+			}
+			users = append(users, userOnce)
+		}
+
+		c.HTML(http.StatusOK, "home.html", gin.H{
+			"title": "여기는 index 입니다.",
+			"users": users,
+		})
 	})
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
-	})
-
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//    "foo":  "bar",
-	//    "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
 	})
 
 	return r
@@ -67,5 +64,5 @@ func setupRouter() *gin.Engine {
 func main() {
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	r.Run(":8081")
 }
